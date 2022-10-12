abf42aa1a101d75d4d9da6a6fff0ea4e4d1087b0
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-12 02:38:43 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/*******************************************************************************
+ * Name: Jason D. F. D'Oyley
+ * Date: Oct. 12, 2022
+ * Assignment CIS214 Week 2 GP - Conditional Loops
+ *
+ * Main application (program) class.
+ * In this application we will demonstrate the use of condition-based
+ * loops by asking the user for input and continuing to ask for input
+ * until the user indicates that they want to quit. Upon quitting, the
+ * program will display the number of passing and failing grades
+ * entered.
+ */
+public class GradeCounts
+{
+    static void Main(string[] args)
+    {
+        // Code here ...
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# Conditional Loops
+
+Main application (program) class.
+In this application we will demonstrate the use of condition-based
+loops by asking the user for input and continuing to ask for input
+until the user indicates that they want to quit. Upon quitting, the
+program will display the number of passing and failing grades
+entered.
